967c8cfed89c86c8e134c73c62d9f8c21cdb24b0
jonorl <39122230+jonorl@users.noreply.github.com>
2024-10-03 09:59:31 +0300

    Added add book form and function logic to push new books into the empty myLibrary array of objects

--- main.js
+++ main.js
@@ -1,13 +1,40 @@
+const myLibrary = [];
+
+const addBookButton = document.querySelector(".add");
+
+// Constructor function
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function() {
-        return (this.title + " by " + this.author + ", " + this.pages + " pages, " + this.read);
-    }
 }
 
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'not read yet');
+function addBookToLibrary(name, author, numberOfPages, read) {
+    let addedBook = new Book(name, author, numberOfPages, read);
+    myLibrary.push(addedBook);
+}
 
-console.log(theHobbit.info());
+function addBookButtonFunction(event) {
+    
+    // This code snippet prevents the add button from sending data to the server
+    event.preventDefault();
+
+    // Take the values from the form and push them to the MyLibrary Array of Objects.
+    let name = document.querySelector(".name").value;
+    let author = document.querySelector(".author").value
+    let numberOfPages = document.querySelector(".numberOfPages").value
+    let read = document.querySelector(".read").checked
+    addBookToLibrary(name, author, numberOfPages, read);
+
+    // Return the values to blank
+    document.querySelector(".name").value = "";
+    document.querySelector(".author").value = "";
+    document.querySelector(".numberOfPages").value = "";
+    document.querySelector(".read").checked = false;
+
+    //display the contents of myLibrary
+    console.table(myLibrary);
+}
+
+addBookButton.addEventListener("click", addBookButtonFunction);
