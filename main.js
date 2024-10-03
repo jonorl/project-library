92a0aa1ac86e4d3d3547ad2c0874cc9a847737ac
jonorl <39122230+jonorl@users.noreply.github.com>
2024-10-03 15:17:55 +0300

    Added the loop to go through myLibrary as indicated by point 3

--- main.js
+++ main.js
@@ -4,16 +4,49 @@ const cardsContainerDiv = document.querySelector(".cardsContainer");
 const addBookButton = document.querySelector(".add");
 
 // Constructor function
-function Book(title, author, pages, read) {
+function Book(id, title, author, pages, read) {
+    this.id = id;
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
 }
 
-function addBookToLibrary(name, author, numberOfPages, read) {
-    let addedBook = new Book(name, author, numberOfPages, read);
+function addBookToLibrary(id, name, author, numberOfPages, read) {
+    let addedBook = new Book(id, name, author, numberOfPages, read);
     myLibrary.push(addedBook);
+}
+
+function loopArray() {
+
+        // delete everything under the CardsContainer div to avoid duplication
+
+        while (cardsContainerDiv.firstChild) {
+            cardsContainerDiv.removeChild(cardsContainerDiv.firstChild);
+        }
+
+        // loop and get all the books from myLibrary and display it in cards
+
+        for (let [index, item] of myLibrary.entries()) {
+            let newCard = document.createElement("div");
+            newCard.className = `card${index}`;
+            newCard.innerHTML = `
+                <div class="bookIndex">Book&nbsp${ + index + 1}</div>
+                <div class="bookName">${item.title}</div>
+                <div class="bookAuthor">${item.author}</div>
+                <div class="bookNumberOfPages">${item.pages}</div>
+                <label for="readCard">Read?</label><input class="readCard" type="checkbox" ${item.read ? 'checked' : ''}><br>
+                <button id="deleteCard" class="delete">X</button>
+                `;
+            cardsContainerDiv.appendChild(newCard);
+
+            // create the delete button and asign a function to delete the card upon clicking
+            const deleteButton = newCard.querySelector("button.delete");
+            deleteButton.addEventListener("click", function() {
+                cardsContainerDiv.removeChild(this.parentElement)
+                myLibrary = myLibrary.filter(item => item.id !== item.id);
+            })
+        }
 }
 
 function addBookButtonFunction(event) {
@@ -26,26 +59,9 @@ function addBookButtonFunction(event) {
     let author = document.querySelector(".author").value
     let numberOfPages = document.querySelector(".numberOfPages").value
     let read = document.querySelector(".read").checked
-    let readChecked = document.querySelector(".read")
-    addBookToLibrary(name, author, numberOfPages, read);
+    addBookToLibrary(myLibrary.length, name, author, numberOfPages, read);
 
-    let highestIndex = myLibrary.length - 1;
-
-    // Create new cards under the cards-container div
-
-    let newCard = document.createElement("div");
-    newCard.className = `card${highestIndex}`;
-    newCard.innerHTML = `
-        <div class="bookIndex">Book&nbsp${ + highestIndex + 1}</div>
-        <div class="bookName">${name}</div>
-        <div class="bookAuthor">${author}</div>
-        <div class="bookNumberOfPages">${numberOfPages}</div>
-        <label for="readCard">Read?</label><input class="readCard" type="checkbox" ${read ? 'checked' : ''}><br>
-        <button id="deleteCard" class="delete">X</button>
-        `;
-    cardsContainerDiv.appendChild(newCard);
-    const deleteButton = newCard.querySelector("button.delete");
-    deleteButton.addEventListener("click", function() { cardsContainerDiv.removeChild(this.parentElement)})
+    loopArray()
 
     // Return the values to blank
     document.querySelector(".name").value = "";
