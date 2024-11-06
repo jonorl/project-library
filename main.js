6cfce33d0f4d5d7ee0270744de9caa3807c07c79
jonorl <39122230+jonorl@users.noreply.github.com>
2024-11-06 17:52:43 +0300

    Name JS validation works, need to complete the other fields and fix CSS

--- main.js
+++ main.js
@@ -2,117 +2,157 @@ let myLibrary = [];
 
 const cardsContainerDiv = document.querySelector(".cardsContainer");
 const addBookButton = document.querySelector(".add");
-const cancelButton = document.querySelector(".cancel")
-const modalButton = document.querySelector("[data-open-modal]")
-const dialog = document.querySelector("dialog")
-const modal = document.querySelector("[data-modal]")
+const cancelButton = document.querySelector(".cancel");
+const modalButton = document.querySelector("[data-open-modal]");
+const dialog = document.querySelector("dialog");
+const modal = document.querySelector("[data-modal]");
+const form = document.querySelector(".addNewBook")
+let nameInput;
+const nameRegex = /^([a-zA-Z]+\s)*[a-zA-Z]+$/;
+let error;
 
 // Constructor function
 
 function Book(id, title, author, pages, read) {
-    this.id = id;
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+  this.id = id;
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
 function addBookToLibrary(id, name, author, numberOfPages, read) {
-    let addedBook = new Book(id, name, author, numberOfPages, read);
-    myLibrary.push(addedBook);
+  let addedBook = new Book(id, name, author, numberOfPages, read);
+  myLibrary.push(addedBook);
 }
 
 function loopArray() {
+  // delete everything under the CardsContainer div to avoid duplication
 
-    // delete everything under the CardsContainer div to avoid duplication
+  while (cardsContainerDiv.firstChild) {
+    cardsContainerDiv.removeChild(cardsContainerDiv.firstChild);
+  }
 
-    while (cardsContainerDiv.firstChild) {
-        cardsContainerDiv.removeChild(cardsContainerDiv.firstChild);
-    }
+  // loop and get all the books from myLibrary and display it in cards
 
-    // loop and get all the books from myLibrary and display it in cards
-
-    for (let [index, item] of myLibrary.entries()) {
-        let newCard = document.createElement("div");
-        newCard.className = `card${index}`;
-        newCard.innerHTML = `
-            <div class="bookIndex">Book&nbsp${ + index + 1}</div>
+  for (let [index, item] of myLibrary.entries()) {
+    let newCard = document.createElement("div");
+    newCard.className = `card${index}`;
+    newCard.innerHTML = `
+            <div class="bookIndex">Book&nbsp${+index + 1}</div>
             <div class="bookName">${item.title}</div>
             <div class="bookAuthor">${item.author}</div>
             <div class="bookNumberOfPages">${item.pages}</div>
-            <label for="readCard">Read?</label><input class="readCard" type="checkbox" ${item.read ? 'checked' : ''}><br>
+            <label for="readCard">Read?</label><input class="readCard" type="checkbox" ${
+              item.read ? "checked" : ""
+            }><br>
             <button id="deleteCard" class="delete">X</button>
             `;
-        cardsContainerDiv.appendChild(newCard);
+    cardsContainerDiv.appendChild(newCard);
 
-        // The read button changes the status on myLibrary
+    // The read button changes the status on myLibrary
 
-        const readButton = newCard.querySelector(".readCard");
-        readButton.addEventListener("click", () => {
-            item.read = !item.read;
-            readButton.checked = item.read;
-        });
+    const readButton = newCard.querySelector(".readCard");
+    readButton.addEventListener("click", () => {
+      item.read = !item.read;
+      readButton.checked = item.read;
+    });
 
-        // create the delete button and asign a function to delete the card upon clicking
+    // create the delete button and asign a function to delete the card upon clicking
 
-        const deleteButton = newCard.querySelector("button.delete");
-        deleteButton.addEventListener("click", function() {
-            cardsContainerDiv.removeChild(this.parentElement)
-            myLibrary = myLibrary.filter(book => book !== item);
-        });
-    }
+    const deleteButton = newCard.querySelector("button.delete");
+    deleteButton.addEventListener("click", function () {
+      cardsContainerDiv.removeChild(this.parentElement);
+      myLibrary = myLibrary.filter((book) => book !== item);
+    });
+  }
 }
 
 function addBookButtonFunction(event) {
+  // This code snippet prevents the add button from sending data to the server
 
-    // This code snippet prevents the add button from sending data to the server
+  event.preventDefault();
 
-    event.preventDefault();
+  validation();
 
-    // Take the values from the form and push them to the MyLibrary Array of Objects.
+  // Take the values from the form and push them to the MyLibrary Array of Objects.
 
-    let name = document.querySelector(".name").value;
-    let author = document.querySelector(".author").value
-    let numberOfPages = document.querySelector(".numberOfPages").value
-    let read = document.querySelector(".read").checked
-    addBookToLibrary(myLibrary.length, name, author, numberOfPages, read);
+  let name = document.querySelector("#name").value;
+  let author = document.querySelector("#author").value;
+  let numberOfPages = document.querySelector("#numberOfPages").value;
+  let read = document.querySelector("#read").checked;
+  addBookToLibrary(myLibrary.length, name, author, numberOfPages, read);
 
-    loopArray()
+  loopArray();
 
-    // Return the values to blank
+  // Return the values to blank
 
-    document.querySelector(".name").value = "";
-    document.querySelector(".author").value = "";
-    document.querySelector(".numberOfPages").value = "";
-    document.querySelector(".read").checked = false;
+  document.querySelector("#name").value = "";
+  document.querySelector("#author").value = "";
+  document.querySelector("#numberOfPages").value = "";
+  document.querySelector("#read").checked = false;
 
-    dialog.close()
+  dialog.close();
 }
 
-addBookButton.addEventListener("click", addBookButtonFunction);
+addBookButton.addEventListener("click", function(event) {
+    addBookButtonFunction(event);
+});
 
 // modals
 
 modalButton.addEventListener("click", (event) => {
-    event.preventDefault();
-    document.querySelector("[data-open-modal]").blur();
-    modal.showModal();
-})
+  event.preventDefault();
+  document.querySelector("[data-open-modal]").blur();
+  modal.showModal();
+
+
+  nameInput = document.querySelector("#name");
+  error = nameInput.nextElementSibling;
+  const isValid = nameInput.value.length === 0 || nameRegex.test(nameInput.value);
+  nameInput.className = isValid ? "valid" : "invalid";
+  nameInput.addEventListener("input", () => {
+    const isValid = nameInput.value.length === 0 || nameRegex.test(nameInput.value);
+    if (isValid) {
+      nameInput.className = "valid";
+      error.textContent = "";
+      error.className = "error";
+    } 
+    else {
+      nameInput.className = "invalid";
+    }
+  });
+});
+
+modal.addEventListener("keydown", validation)
 
 cancelButton.addEventListener("click", (event) => {
-    event.preventDefault();
-    dialog.close();
-})
+  event.preventDefault();
+  dialog.close();
+});
 
-dialog.addEventListener("mousedown", e => {
-    const dialogDimensions = dialog.getBoundingClientRect()
-    if (
-      e.clientX < dialogDimensions.left ||
-      e.clientX > dialogDimensions.right ||
-      e.clientY < dialogDimensions.top ||
-      e.clientY > dialogDimensions.bottom
-    ) {
-      e.preventDefault();
-      dialog.close()
+dialog.addEventListener("mousedown", (e) => {
+  const dialogDimensions = dialog.getBoundingClientRect();
+  if (
+    e.clientX < dialogDimensions.left ||
+    e.clientX > dialogDimensions.right ||
+    e.clientY < dialogDimensions.top ||
+    e.clientY > dialogDimensions.bottom
+  ) {
+    e.preventDefault();
+    dialog.close();
+  }
+});
+
+function validation () {
+    const isValid = nameInput.value.length === 0 || nameRegex.test(nameInput.value);
+    if (!isValid) {
+        nameInput.className = "invalid";
+        error.textContent = "Invalid name!";
+        error.className = "error active";
+    } else {
+        nameInput.className = "valid";
+        error.textContent = "";
+        error.className = "error";
     }
-  })
+}
